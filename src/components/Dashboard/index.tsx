b4c83cabac6cfa66d25a6b8faae2237eabb8e69f
tamaracosta <tamarasantosc@gmail.com>
2021-12-06 15:57:21 -0300

    Inserindo TransationTable no Dashboard

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles"
 import { Summary } from "../Summary"
+import { TransationTable } from "../TransactionsTable"
 
 export function Dashboard(){
   return (
     <Container>
       <Summary />
+      <TransationTable />
     </Container>
 
   )
