225c0c9684aa6b88f7203ae4882618dbc15825c1
tamaracosta <tamarasantosc@gmail.com>
2021-12-07 11:41:44 -0300

    inserindo TransactionsProvider no APP

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import { useState } from "react";
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import Modal from 'react-modal';
+import { TransactionsProvider } from './TransactionsContext'
 
 Modal.setAppElement('#root');
 export function App() {
@@ -19,7 +20,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
      <NewTransactionModal 
@@ -27,7 +28,7 @@ export function App() {
        onRequestClose={handleCloseNewTransactionModal }
       />
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
